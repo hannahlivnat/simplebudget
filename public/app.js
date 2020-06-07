790777342372a5721756a6e50bf7014bb8fed3e4
Hannah L <hannahlivnat@gmail.com>
2020-06-07 15:23:49 -0700

    added options to materialize datepicker that programmatically filter calendar to only allow user to pick from date in current month

--- public/app.js
+++ public/app.js
@@ -1,8 +1,33 @@
 $(() => {
+  //get dates for the datepicker input
+
+
+
+
   $('select').formSelect();
+
   $(".dropdown-trigger").dropdown();
-  $('.datepicker').datepicker({
-    format: 'mm-dd-yyyy'
+
+  //interactive date picker
+  //https://www.geeksforgeeks.org/how-to-get-the-number-of-days-in-a-specified-month-using-javascript/
+
+
+  const daysinMonth = (month, year) => {
+    return new Date(year, month + 1, 0).getDate();
+  };
+
+  const currentMonth = new Date().getMonth()
+  const currentYear = new Date().getFullYear()
+
+  const lastDay = daysinMonth(currentMonth, currentYear);
+  console.log(lastDay);
+
+
+  $(".datepicker").datepicker({
+    minDate: new Date(currentYear, currentMonth, 1),
+    maxDate: new Date(currentYear, currentMonth, lastDay),
+    format: 'mm-dd-yyyy',
+    showMonthAfterYear: true
   });
 
   //tabs
